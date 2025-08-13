2e3fbe16552012b88165c8480ee51b94652bda61
sharonthy97 <sharon.thy97@gmail.com>
2025-08-13 16:17:35 +0800

    improved the User Experience

--- src/index.js
+++ src/index.js
@@ -14,9 +14,13 @@ function generateRecipe(event) {
   let searchInput = document.querySelector("#search-input");
   let apiKey = "48ab3c475703c305f36e50o053ta6dba";
   let context =
-    "You are an AI recipe assistant. Please be short, simple and easy to make for beginners. Only include recipe name, ingredients and instructions in this format. Example <ul>list of ingredients</ul> <P>instructions</p>. Sign the recipe with `SheCodes AI` inside <strong> element ";
+    "You are an AI recipe assistant. Please be short, simple and easy to make for beginners. Only include recipe name, ingredients and instructions in this format. Example <h2>Recipe name</h2> <ul>list of ingredients</ul> <P>instructions</p>. Sign the recipe with `SheCodes AI` inside <strong> element ";
   let prompt = `Generate a recipe about ${searchInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="blink">🔥🧑‍🍳Generating a recipe about ${searchInput.value}</div>`;
 
   console.log("Generating recipe");
   console.log(`Prompt: ${prompt}`);
